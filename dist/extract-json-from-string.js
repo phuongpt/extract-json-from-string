6c35ea71f0fc1b1b416779e33cca9c61c43c6d21
Phuong Phan <phuongphantai@gmail.com>
2023-05-31 17:22:11 +0700

    Use 'best-effort-json-parser' to fix JSON value

--- dist/extract-json-from-string.js
+++ dist/extract-json-from-string.js
@@ -66,14 +66,16 @@ window["extractJson"] =
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+var JSONParser = __webpack_require__(1);
 
 var jsonify = function jsonify(almostJson) {
   try {
     return JSON.parse(almostJson);
   } catch (e) {
     almostJson = almostJson.replace(/([a-zA-Z0-9_$]+\s*):/g, '"$1":').replace(/'([^']+?)'([\s,\]\}])/g, '"$1"$2');
-    return JSON.parse(almostJson);
+    return JSONParser.parse(almostJson);
   }
 };
 
@@ -141,6 +143,181 @@ module.exports = function (str) {
 
   return objects;
 };
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.parse = void 0;
+function parse(s) {
+    try {
+        return JSON.parse(s);
+    }
+    catch (e) {
+        const [data, reminding] = parseAny(s, e);
+        parse.lastParseReminding = reminding;
+        if (parse.onExtraToken && reminding.length > 0) {
+            parse.onExtraToken(s, data, reminding);
+        }
+        return data;
+    }
+}
+exports.parse = parse;
+(function (parse) {
+    parse.onExtraToken = (text, data, reminding) => {
+        console.error('parsed json with extra tokens:', {
+            text,
+            data,
+            reminding,
+        });
+    };
+})(parse = exports.parse || (exports.parse = {}));
+function parseAny(s, e) {
+    const parser = parsers[s[0]];
+    if (!parser) {
+        console.error(`no parser registered for ${JSON.stringify(s[0])}:`, { s });
+        throw e;
+    }
+    return parser(s, e);
+}
+const parsers = {};
+function skipSpace(s) {
+    return s.trimLeft();
+}
+parsers[' '] = parseSpace;
+parsers['\r'] = parseSpace;
+parsers['\n'] = parseSpace;
+parsers['\t'] = parseSpace;
+function parseSpace(s, e) {
+    s = skipSpace(s);
+    return parseAny(s, e);
+}
+parsers['['] = parseArray;
+function parseArray(s, e) {
+    s = s.substr(1); // skip starting '['
+    const acc = [];
+    s = skipSpace(s);
+    for (; s.length > 0;) {
+        if (s[0] === ']') {
+            s = s.substr(1); // skip ending ']'
+            break;
+        }
+        const res = parseAny(s, e);
+        acc.push(res[0]);
+        s = res[1];
+        s = skipSpace(s);
+        if (s[0] === ',') {
+            s = s.substring(1);
+            s = skipSpace(s);
+        }
+    }
+    return [acc, s];
+}
+for (const c of '0123456789.-'.slice()) {
+    parsers[c] = parseNumber;
+}
+function parseNumber(s) {
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (parsers[c] === parseNumber) {
+            continue;
+        }
+        const num = s.substring(0, i);
+        s = s.substring(i);
+        return [numToStr(num), s];
+    }
+    return [numToStr(s), ''];
+}
+function numToStr(s) {
+    if (s === '-') {
+        return -0;
+    }
+    const num = +s;
+    if (Number.isNaN(num)) {
+        return s;
+    }
+    return num;
+}
+parsers['"'] = parseString;
+function parseString(s) {
+    for (let i = 1; i < s.length; i++) {
+        const c = s[i];
+        if (c === '\\') {
+            i++;
+            continue;
+        }
+        if (c === '"') {
+            const str = s.substring(0, i + 1);
+            s = s.substring(i + 1);
+            return [JSON.parse(str), s];
+        }
+    }
+    return [JSON.parse(s + '"'), ''];
+}
+parsers['{'] = parseObject;
+function parseObject(s, e) {
+    s = s.substr(1); // skip starting '{'
+    const acc = {};
+    s = skipSpace(s);
+    for (; s.length > 0;) {
+        if (s[0] === '}') {
+            s = s.substr(1); // skip ending '}'
+            break;
+        }
+        const keyRes = parseAny(s, e);
+        const key = keyRes[0];
+        s = keyRes[1];
+        s = skipSpace(s);
+        if (s[0] !== ':') {
+            acc[key] = undefined;
+            break;
+        }
+        s = s.substr(1); // skip ':'
+        s = skipSpace(s);
+        if (s.length === 0) {
+            acc[key] = undefined;
+            break;
+        }
+        const valueRes = parseAny(s, e);
+        acc[key] = valueRes[0];
+        s = valueRes[1];
+        s = skipSpace(s);
+        if (s[0] === ',') {
+            s = s.substr(1);
+            s = skipSpace(s);
+        }
+    }
+    return [acc, s];
+}
+parsers['t'] = parseTrue;
+function parseTrue(s, e) {
+    return parseToken(s, `true`, true, e);
+}
+parsers['f'] = parseFalse;
+function parseFalse(s, e) {
+    return parseToken(s, `false`, false, e);
+}
+parsers['n'] = parseNull;
+function parseNull(s, e) {
+    return parseToken(s, `null`, null, e);
+}
+function parseToken(s, tokenStr, tokenVal, e) {
+    for (let i = tokenStr.length; i >= 1; i--) {
+        if (s.startsWith(tokenStr.slice(0, i))) {
+            return [tokenVal, s.slice(i)];
+        }
+    }
+    /* istanbul ignore next */
+    {
+        const prefix = JSON.stringify(s.slice(0, tokenStr.length));
+        console.error(`unknown token starting with ${prefix}:`, { s });
+        throw e;
+    }
+}
+//# sourceMappingURL=parse.js.map
 
 /***/ })
 /******/ ]);
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ require('file-manifest').generate('./gulp', { match: '*.js' });
 
 gulp.task('cover', gulp.series('clean:coverage', 'spawn:nyc'));
 gulp.task('ci', gulp.series(gulp.parallel('lint', 'cover', 'phantom'), 'codeclimate'));
-gulp.task('test', gulp.series('cover', 'browser'));
+gulp.task('test', gulp.series('cover'));
 gulp.task('unit', gulp.series('spawn:nyc'));
 gulp.task('default', gulp.series('lint', 'test'));
 gulp.task('build', gulp.series('clean:dist', 'spawn:webpack'));
